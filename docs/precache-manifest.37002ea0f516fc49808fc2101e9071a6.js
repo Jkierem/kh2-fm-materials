e2d2a05451d33b66efe2e1c7e9311e83184aca13
Jkierem <juanmiguelgomez8@gmail.com>
2020-08-21 20:32:39 -0500

    Trying deploy

---
+++ docs/precache-manifest.37002ea0f516fc49808fc2101e9071a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9125ebf3ff9402a0054cc35e06249c2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f61986b1e70b641353fa",
+    "url": "/static/css/main.e082dd39.chunk.css"
+  },
+  {
+    "revision": "b91e616a5b27fbde73a2",
+    "url": "/static/js/2.6d16358d.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/static/js/2.6d16358d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f61986b1e70b641353fa",
+    "url": "/static/js/main.184edc20.chunk.js"
+  },
+  {
+    "revision": "b86bf7c51ddd7d42f419",
+    "url": "/static/js/runtime-main.4fe12e95.js"
+  }
+]);
